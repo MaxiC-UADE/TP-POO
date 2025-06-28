03effe6a12a86f4bda262aa3499ae3548517d571
Roberto <aucatoma01@gmail.com>
2025-06-28 11:16:25 -0300

    1 o 2 para ser capitan o no antes de login o register. dependientemente de la opcion ingresada, se instancia un capitan o un jugador.

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int opcionCrearEquipo;
+        // bucle que determina si el usuario quiere ser capitan o un simple jugador.
+        while (true) {
+            System.out.println("¿Desea crear un equipo? Ingrese 1 para aceptar o 2 para rechazar.");
+            opcionCrearEquipo = sc.nextInt(); // lee solo el numero, no el salto de linea que queda en el buffer de entrada
+            sc.nextLine(); // consume el salto de linea, es decir, limpia el buffer de entrada.
 
-        System.out.println("PRESIONE 1 PARA LOGIN 2 PARA REGISTER:");
-        int presion1 = sc.nextInt();
+            if (opcionCrearEquipo == 1 || opcionCrearEquipo == 2){
+                System.out.println("Opcion tomada.");
+                break;
+            }else{
+                System.out.println("Opcion invalida. Por favor ingrese una opcion valida.");
+            }
+
+        }
+
+        // bucle que pregunta al usuario si quiere loguearse o registrarse y actua en consecuencia.
+
+        while(true){
+        System.out.println("Ingrese 1 para logearse o 2 para registrarse.");
+        int opcionLoginRegistro = sc.nextInt();
         sc.nextLine();
 
-        if (presion1 == 1) {
-            /// Login
-            Jugador jugador = new Jugador();
-            jugador.login();
+        if (opcionLoginRegistro == 1) {
+            if (opcionCrearEquipo == 1) {
+                Usuario persona = new Jugador();
+                persona.login();
+            }else{
+                Usuario persona = new Capitan();
+                persona.login();
+                }
+            break;
 
+        } else if(opcionLoginRegistro == 2){
+            if (opcionCrearEquipo == 1) {
+                Usuario persona = new Jugador();
+                persona.registrar();
+            }else{
+                Usuario persona = new Capitan();
+                persona.registrar();
+            }
+            break;
 
-        } else if(presion1 == 2){
-            /// Register
-            Jugador jugador = new Jugador();
-            jugador.registrar(); /// Llama metodo de usuario y guarda la info en un txt.
+        }else{
+            System.out.println("Opcion invalida. Por favor ingrese una opcion valida.");
+        }
 
         }
 
